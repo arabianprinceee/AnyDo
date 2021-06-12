743a445c9ce6a19c2848b547eeae95e2a4cc2bfe
Anas Ben Mustafa <57250290+arabianprince23@users.noreply.github.com>
2021-06-12 14:56:43 +0300

    added full interface skeleton

--- AnyDo/LifeCycle/SceneDelegate.swift
+++ AnyDo/LifeCycle/SceneDelegate.swift
@@ -17,6 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let ToDoVC = ToDoViewController()
+        self.window?.rootViewController = ToDoVC
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
